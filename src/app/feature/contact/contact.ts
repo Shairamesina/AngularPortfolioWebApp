53b12631a9df5e571a128bbfc4acd26754945bab
Shaira Mesina <mesinashaira@gmail.com>
2026-08-21 21:37:34 -0400

    Fix contract form warnings

--- src/app/feature/contact/contact.ts
+++ src/app/feature/contact/contact.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
 
 @Component({
-  imports: [],
   selector: 'app-contact',
-  styleUrl: './contact.scss',
+  imports: [],
   templateUrl: './contact.html',
+  styleUrl: './contact.scss'
 })
 export class Contact {
-  
+
+  sendEmail(event: Event): void {
+    event.preventDefault();
+
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
+
+    const name = formData.get('name');
+    const email = formData.get('email');
+    const subject = formData.get('subject');
+    const message = formData.get('message');
+
+    const body = `
+          Name: ${name}
+          Email: ${email}
+          Subject: ${subject}
+          Message: ${message}
+        `;
+    window.location.href =
+      `mailto:mesinashaira@gmail.com` +
+      `?subject=${encodeURIComponent(String(subject))}` +
+      `&body=${encodeURIComponent(body)}`;
+  }
 }
